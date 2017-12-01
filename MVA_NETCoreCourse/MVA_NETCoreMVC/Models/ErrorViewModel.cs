45c64403f9728fb610cd922882de1b70b3b64629
Matejkos <matejkos@gmail.com>
2017-12-01 21:53:49 +0100

    New MVC project. Not like in course as separate solution but added to main solution

---
+++ MVA_NETCoreCourse/MVA_NETCoreMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVA_NETCoreMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
