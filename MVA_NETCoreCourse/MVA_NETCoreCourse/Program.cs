8fa9712344cf1872e666df5ae33dc60b3801c1e2
Matejkos <matejkos@gmail.com>
2017-12-01 20:09:18 +0100

    Initail MVA .Net Core course

---
+++ MVA_NETCoreCourse/MVA_NETCoreCourse/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ *  MVA .Net Core course 
+ *  Mateusz Stanek 
+ *  File: Program.cs
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace MVA_NETCoreCourse
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:8090")
+                .UseStartup<Startup>()
+                .UseIISIntegration()
+                .Build();
+    }
+}
