bf3374af578626e3d270d20a35694961277176ae
Matejkos <matejkos@gmail.com>
2017-12-05 21:03:13 +0100

    Finished introducing to Rrouting and MVC

---
+++ MVA_NETCoreCourse/MVA_NETCoreMVC/Controllers/TicketsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVA_NETCoreMVC.Models;
+
+namespace MVA_NETCoreMVC.Controllers
+{
+    public class TicketsController : Controller
+    {
+        public ActionResult Index()
+        {
+            var s = new Seat() { Location = "Orchestra", Price =300.00 };
+            
+            return View(s);
+        }
+    }
+}
